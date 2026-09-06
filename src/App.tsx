07bf0b22d21a587835d67ab4effd1c7762042c55
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-09-05 19:10:32 -0500

    feat: agregar RuterProvider en App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import './App.css'
+import { RouterProvider } from "react-router-dom"
+import { router } from "./app/router"
 
 function App() {
-
-  return (
-    <h1></h1>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
