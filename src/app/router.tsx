4f24fa246d720276dee7230d85a735d16d5f272d
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-09-06 14:49:28 -0500

    feat: agregar ruta para la recuperacion de cuenta

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -2,9 +2,11 @@ import { createBrowserRouter } from "react-router-dom"
 import { Home } from "@/pages/Home"
 import { Login } from "@/pages/Login"
 import { Register } from "@/pages/Register"
+import { ForgotPassword } from "@/pages/ForgotPassword"
 
 export const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/login", element: <Login /> },
   { path: "/register", element: <Register /> },
+  { path: "/forgot-password", element: <ForgotPassword /> },
 ])
