81704963a0473bb32ffba9795c5ff7d0badfc30f
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-09-09 19:36:06 -0500

    feat: crear ruta para el acceso a Task

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -3,10 +3,12 @@ import { Home } from "@/pages/Home"
 import { Login } from "@/pages/Login"
 import { Register } from "@/pages/Register"
 import { ForgotPassword } from "@/pages/ForgotPassword"
+import { Tasks } from "@/pages/Tasks"
 
 export const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/login", element: <Login /> },
   { path: "/register", element: <Register /> },
   { path: "/forgot-password", element: <ForgotPassword /> },
+  { path: "/tasks", element: <Tasks /> },
 ])
