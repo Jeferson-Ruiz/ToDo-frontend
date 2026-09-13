1e07a6eaf99965ac62dfa119713c3278852d6a07
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-09-13 11:21:14 -0500

    feat: extender interfaz TaskEssential

--- src/features/task/types/taskEssential.ts
+++ src/features/task/types/taskEssential.ts
@@ -4,8 +4,10 @@ export type TaskPriority = "BAJA" | "MEDIA" | "ALTA"
 export interface TaskEssential {
   id?: string | number
   title: string
+  description?: string | null
   category?: string | null
   deadline?: string | null
+  dateCreation?: string | null
   status?: TaskStatus | null
   priority?: TaskPriority | null
 }
